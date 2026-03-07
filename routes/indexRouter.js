8b2672420e894d9d9ffb976a0f611319407cf251
Prak005 <prakharmallick005@gmail.com>
2026-03-07 21:09:37 +0530

    add route for create item

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,5 +7,7 @@ const itemsController = require('../controllers/itemsController');
 indexRouter.get('/', categoryController.categoryList);
 indexRouter.get('/category/:id', categoryController.categoryItems);
 indexRouter.get('/items/:id', itemsController.itemDetail);
+indexRouter.get('/items/create', itemsController.createItemGet);
+indexRouter.get('/items/create', itemsController.createItemPost);
 
 module.exports = indexRouter;
