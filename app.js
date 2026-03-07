ad965e8a763cb24cc75beba3cf56710778121187
Prak005 <prakharmallick005@gmail.com>
2026-03-07 13:14:34 +0530

    configure base app

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const indexRouter = require('./routes/indexRouter');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expresss.urlencoded({ extended:true }));
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+    if(error)
+        throw error;
+    console.log(`Listening on ${PORT}\nhttp://localhost:${PORT}`);
+});
