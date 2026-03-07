ef00aae5379660ca1f2b9bde0dd1cf9f6c995848
Prak005 <prakharmallick005@gmail.com>
2026-03-07 13:15:26 +0530

    create pool

---
+++ db/pool.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+module.exports = new Pool ({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+});
