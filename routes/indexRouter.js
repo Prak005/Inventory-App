76ae2e00c59060e4e39d576e6a1c7810a072ab11
Prak005 <prakharmallick005@gmail.com>
2026-03-07 20:18:53 +0530

    add items route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const indexRouter = Router();
 
 const categoryController = require('../controllers/categoryController');
+const itemsController = require('../controllers/itemsController');
 
 indexRouter.get('/', categoryController.categoryList);
 indexRouter.get('/category/:id', categoryController.categoryItems);
+indexRouter.get('/items/:id', itemsController.itemDetail);
 
 module.exports = indexRouter;
