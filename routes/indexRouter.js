882451616c4ebe71261aab85889231c562c05a6a
Prak005 <prakharmallick005@gmail.com>
2026-03-08 02:10:38 +0530

    add updateItem router

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,8 +6,13 @@ const itemsController = require('../controllers/itemsController');
 
 indexRouter.get('/', categoryController.categoryList);
 indexRouter.get('/category/:id', categoryController.categoryItems);
+
 indexRouter.get('/items/create', itemsController.createItemGet);
 indexRouter.post('/items/create', itemsController.createItemPost);
+
+indexRouter.get('/items/:id/update', itemsController.updateItemGet);
+indexRouter.post('/items/:id/update', itemsController.updateItemPost);
+
 indexRouter.get('/items/:id', itemsController.itemDetail);
 
 module.exports = indexRouter;
