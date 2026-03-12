d97122edc623ee8b77e88bf60e53b5bbc8952680
Prak005 <prakharmallick005@gmail.com>
2026-03-12 15:44:34 +0530

    add to use public dir

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require('path');
 const indexRouter = require('./routes/indexRouter');
 
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended:true }));
